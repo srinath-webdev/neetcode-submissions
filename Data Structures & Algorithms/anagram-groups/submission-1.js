cc3e40b13e1742b2218daa8fc7f15b2fdf55b1d3
Srinath_Manju <87973513+srinath-webdev@users.noreply.github.com>
2026-06-14 23:25:01 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = new Map();
+
+        for(let word of strs){
+            let uniqueWord = word.split("").sort().join("") // sort will "eat" to "aet"
+
+            if(!map[uniqueWord]){
+                map[uniqueWord] = []
+            }
+
+            map[uniqueWord].push(word)
+        }
+
+        return Object.values(map)
+    }
+}
