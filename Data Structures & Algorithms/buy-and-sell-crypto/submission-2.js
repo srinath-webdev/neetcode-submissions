7eeed974a4b5a52fa5090c8c8a96fc0e972440b9
Srinath_Manju <87973513+srinath-webdev@users.noreply.github.com>
2026-06-24 14:13:04 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        
+        let buy = 0 ; 
+        let sell = 1 ;
+
+        let maxProfit  = 0 ; 
+
+
+        while(sell < prices.length){
+
+            
+            if(prices[sell] > prices[buy]){
+
+            let profit = prices[sell] - prices[buy];
+
+            maxProfit = Math.max(maxProfit , profit); 
+
+            }
+            else{
+                buy = sell 
+            }
+
+            sell++
+        }
+
+
+        return maxProfit
+
+    }
+}
